a53900cfa7f3b9bf13084bab3af8f3ebd367ffe2
archanadoshi <adoshi@solstice.com>
2019-09-06 15:00:30 -0500

    first commit to rabbitmq publisher

---
+++ src/main/java/com/examplerabbitmq/rabbitmqpublisher/ExchangeRate.java
@@ -0,0 +1,18 @@
+package com.examplerabbitmq.rabbitmqpublisher;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ExchangeRate implements Serializable {
+    private String fromCurrency;
+    private String toCurrency;
+}
---
+++ src/test/java/com/examplerabbitmq/rabbitmqpublisher/RabbitmqpublisherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.examplerabbitmq.rabbitmqpublisher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqpublisherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
